aeeec766b64dfd58667015d18dd1b1abc94adc0c
Vinícius Fraga <vinicius.vmsf@gmail.com>
2016-01-08 03:54:51 -0300

    Refactored gateway code.

--- gateways/gatewayExample/gateway_example.py
+++ gateways/gatewayExample/gateway_example.py
@@ -3,73 +3,21 @@ from time import sleep
 import json
 import random
 import semantics.simulator.deviceSimulator as dev
-import semantics.simulator.sense as sense
-import semantics.simulator.actuators as act
 import semantics.simulator.helpers as helpers
 
-variables = {}
-for i in range(len(helpers.variableIds)):
-    variables[helpers.variableIds[i]] = sense.createVariable(
-        helpers.variableIds[i])
-
-units = {}
-for i in range(len(helpers.unitIds)):
-    units[helpers.unitIds[i]] = sense.createUnit(helpers.unitIds[i],
-                                                 helpers.variableIds[i])
-
-sensors = {}
-for i in range(len(helpers.sensorsIds)):
-    if i%2 == 0:
-        sensors[helpers.sensorsIds[i]] = sense.createContinuousSensor(
-            helpers.sensorsIds[i],
-            helpers.unitIds[0]
-        )
-    else:
-        sensors[helpers.sensorsIds[i]] = sense.createDiscreteSensor(
-            helpers.sensorsIds[i],
-            helpers.variableIds[i]
-        )
-
-actuators = {}
-for i in range(len(helpers.actuatorsIds)):
-    if i%2 == 0:
-        actuators[helpers.actuatorsIds[i]] = act.createContinuousActuator(
-            helpers.actuatorsIds[i],
-            helpers.variableIds[i]
-        )
-    else:
-        actuators[helpers.actuatorsIds[i]] = act.createDiscreteActuator(
-            helpers.actuatorsIds[i],
-            helpers.variableIds[i]
-        )
-
-platforms = {}
-for i in range(len(helpers.platformIds)):
-    platforms[helpers.platformIds[i]] = dev.createPlatform(
-        helpers.platformIds[i], helpers.sensorsIds[i], helpers.actuatorsIds[i]
-    )
-
-devices = {}
-for i in range(len(helpers.deviceIds)):
-    devices[helpers.deviceIds[i]] = dev.createDevice(helpers.deviceIds[i],i%2)
-
-measurements = {}
-
-collections = [measurements, devices, platforms, sensors, actuators, units,
-               variables]
-types = ['Measurement', 'Device', 'Platform', 'Sensor', 'Actuator', 'Unit',
-         'Variable']
+collections = [helpers.devices, helpers.platforms, helpers.sensors,
+               helpers.actuators, helpers.units, helpers.variables]
+types = ['Device', 'Platform', 'Sensor', 'Actuator', 'Unit', 'Variable']
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("gateways/broker")
 
 def on_message(client, userdata, msg):
-    print(msg.payload.decode())
     obj = json.loads(msg.payload.decode())
     if obj['type'] in types:
         index = len(types) - 1
-        while index >= 0:
+        while index >= types.index(obj['type']):
             print("index = " + str(index))
             for key in collections[index]:
                 response = collections[index][key]
@@ -89,7 +37,7 @@ client.loop_start()
 
 index = 0
 while True:
-    for id in devices:
+    for id in helpers.devices:
         sleep(1)
         if index%2 == 0:
             value = random.random() * 100
@@ -97,8 +45,9 @@ while True:
         else:
             value = True
             index = 0
-        measurement = dev.createMeasurement(devices[id]["@id"], value)
+        measurement = dev.createMeasurement(helpers.globalUrl,
+                                            helpers.devices[id]["id"], value)
+        print(measurement)
         client.publish("gateways/test", json.dumps(measurement))
 
 client.loop_stop()
-
